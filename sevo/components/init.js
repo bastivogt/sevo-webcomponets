8ac79489616cadb95ed2faaa1fcc7144f0c3b01c
Sevo <betont.online@googlemail.com>
2023-11-15 01:59:06 +0100

    add sevo-main-nav overlay menu

--- sevo/components/init.js
+++ sevo/components/init.js
@@ -6,3 +6,4 @@ import "./sevo-2-columns.js";
 import "./sevo-3-columns.js";
 import "./sevo-4-columns.js";
 import "./sevo-card.js";
+import "./sevo-main-nav.js";
---
+++ sevo/components/sevo-main-nav.js
@@ -0,0 +1,186 @@
+"use-strict";
+
+const template = document.createElement("template");
+template.innerHTML = /*html*/ `
+    <style>
+
+        * {
+            box-sizing: border-box;
+        }
+        #sevo-main-nav-container {
+            padding: 10px 10px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            
+        }
+
+        #sevo-main-nav-trigger {
+            cursor: pointer;
+        }
+
+        #sevo-main-nav-overlay {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100vh;
+            z-index: 9999;
+            background-color: rgba(0, 0, 0, .9);
+            
+        }
+
+
+
+
+
+
+
+        #sevo-main-nav-content-container {
+            position: relative;
+            display: flex;
+            justify-content: center;  
+            align-items: center;
+            /*background-color: red;*/
+            height: 100%;
+            overflow-y: auto;
+            padding: 10px;
+        }
+
+        #sevo-main-nav-close-trigger {
+            position: absolute;
+            top: 10px;
+            right: 10px;
+            cursor: pointer;
+        }
+
+
+        #sevo-main-nav-overlay-content {
+            
+        }
+
+
+
+        .display-none {
+            display: none !important;
+        }
+
+
+    </style>
+    <section id="sevo-main-nav-container">
+        <div id="sevo-main-nav-logo"><slot name="logo"></slot></div>
+        <div id="sevo-main-nav-trigger"><slot name="trigger"></slot></div>
+    </section>
+    <section id="sevo-main-nav-overlay" class="display-none">
+        
+        <div id="sevo-main-nav-content-container">
+            <div id="sevo-main-nav-close-trigger"><slot name="close-trigger"></slot></div>
+            <div id="sevo-main-nav-overlay-content"><slot name="content"></slot></div>
+        </div>
+    </section>
+`;
+
+class SevoMainNav extends HTMLElement {
+  constructor() {
+    super();
+
+    this.root = this.attachShadow({ mode: "closed" });
+    this.root.appendChild(template.content.cloneNode(true));
+
+    this.elements = {
+      container: this.root.querySelector("#sevo-main-nav-container"),
+      logo: this.root.querySelector("#sevo-main-nav-logo"),
+      trigger: this.root.querySelector("#sevo-main-nav-trigger"),
+      overlay: this.root.querySelector("#sevo-main-nav-overlay"),
+      closeContainer: this.root.querySelector("#sevo-main-nav-close-container"),
+      closeTrigger: this.root.querySelector("#sevo-main-nav-close-trigger"),
+      overlayContent: this.root.querySelector("#sevo-main-nav-overlay-content"),
+    };
+  }
+
+  // observedAttributes
+  static get observedAttributes() {
+    return [
+      "bar-background-color",
+      "bar-color",
+      "overlay-background-color",
+      "overlay-color",
+    ];
+  }
+
+  // barBackgroundColor
+  get barBackgroundColor() {
+    return this.getAttribute("bar-background-color");
+  }
+  set barBackgroundColor(value) {
+    this.setAttribute("bar-background-color", value);
+  }
+
+  // barColor
+  get barColor() {
+    return this.getAttribute("bar-color");
+  }
+  set barColor(value) {
+    this.setAttribute("bar-color", value);
+  }
+
+  // overlayBackgroundColor
+  get overlayBackgroundColor() {
+    return this.getAttribute("overlay-background-color");
+  }
+  set overlayBackgroundColor(value) {
+    this.setAttribute("overlay-background-color", value);
+  }
+
+  // overlayColor
+  get overlayColor() {
+    return this.getAttribute("overlay-color");
+  }
+  set overlayColor(value) {
+    this.setAttribute("overlay-color", value);
+  }
+
+  // connectedCallback
+  connectedCallback() {
+    // trigger
+    this.elements.trigger.addEventListener("click", () => {
+      console.log("SevoMainNav", "trigger clicked");
+      this.elements.overlay.classList.remove("display-none");
+      document.body.style["overflow-y"] = "hidden";
+    });
+
+    // close-trigger
+    this.elements.closeTrigger.addEventListener("click", () => {
+      console.log("SevoMainNav", "closeTrigger clicked");
+      this.elements.overlay.classList.add("display-none");
+      document.body.style["overflow-y"] = "scroll";
+    });
+  }
+
+  // attributeChangedCallback
+  attributeChangedCallback() {
+    // bar-background-color
+    if (this.barBackgroundColor) {
+      this.elements.container.style["background-color"] =
+        this.barBackgroundColor;
+    }
+
+    // bar-color
+    if (this.barColor) {
+      this.elements.container.style["color"] = this.barColor;
+    }
+
+    // overlay-background-color
+    if (this.overlayBackgroundColor) {
+      this.elements.overlay.style["background-color"] =
+        this.overlayBackgroundColor;
+    }
+
+    // overlay-color
+    if (this.overlayColor) {
+      this.elements.overlay.style["color"] = this.overlayColor;
+    }
+  }
+}
+
+window.customElements.define("sevo-main-nav", SevoMainNav);
